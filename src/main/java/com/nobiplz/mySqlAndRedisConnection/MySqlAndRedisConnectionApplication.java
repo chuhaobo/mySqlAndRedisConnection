71cbc1d3e59d9ab0381c4fb8cf3b9839194dd833
chuhaobo <543148817@qq.com>
2020-05-24 01:18:13 +0800

    add: 1. redis update

--- src/main/java/com/nobiplz/mySqlAndRedisConnection/MySqlAndRedisConnectionApplication.java
+++ src/main/java/com/nobiplz/mySqlAndRedisConnection/MySqlAndRedisConnectionApplication.java
@@ -3,9 +3,10 @@ package com.nobiplz.mySqlAndRedisConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
-
+@EnableCaching
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 /*@ComponentScan(basePackages ={ "com.nobiplz.mySqlAndRedisConnection"})*/
 public class MySqlAndRedisConnectionApplication {
---
+++ src/main/java/com/nobiplz/mySqlAndRedisConnection/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.nobiplz.mySqlAndRedisConnection.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+  //设置过期时间
+  //Duration 为时间间隔
+  private Duration  timeValue = Duration.ofDays(1);
+
+  @Bean(name = "RedisTemplate")
+  public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+    RedisTemplate<Object,Object> template = new RedisTemplate<>();
+    //设置为当前redis链接
+    template.setConnectionFactory(redisConnectionFactory);
+    template.setKeySerializer(keySerializer());
+    template.setHashKeySerializer(keySerializer());
+    template.setValueSerializer(valueSerializer());
+    template.setHashValueSerializer(valueSerializer());
+    return template;
+  }
+
+  private RedisSerializer<String> keySerializer() {
+    return new StringRedisSerializer();
+  }
+  private RedisSerializer<Object> valueSerializer() {
+    return new GenericJackson2JsonRedisSerializer();
+  }
+
+  @Bean
+  public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    //默认1
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+      //设置过期时间
+      .entryTtl(this.timeValue)
+      .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+      .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()))
+      .disableCachingNullValues();
+    RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
+      .cacheDefaults(config)
+      .transactionAware()
+      .build();
+    return redisCacheManager;
+  }
+}
